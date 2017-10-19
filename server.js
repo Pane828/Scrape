126adc397cda00787f19ced657d2c1a69e4a0417
unknown <pane828@gmail.com>
2017-10-19 19:54:14 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var mongojs = require("mongojs");
+var request = require("request");
+var cheerio = require("cheerio");
+var app = express();
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+app.get("/", function(req, res) {
+  res.send("Scrape App");
+});
+
+app.get("/all", function(req, res) {
+  db.scrapedData.find({}, function(error, found) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      res.json(found);
+    }
+  });
+});
+
+app.get("/scrape", function(req, res) {
+  request("https://www.reddit.com/r/AskReddit/", function(error, response, html) {
+    var $ = cheerio.load(html);
+    $(".title").each(function(i, element) {
+      var title = $(element).children("a").text();
+      var link = $(element).children("a").attr("href");
+      console.log(title);
+      console.log(link);
+
+      if (title || link) {
+        db.scrapedData.insert({
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log(inserted);
+          }
+        });
+      }
+    });
+  });
+  res.send("Scrape Complete");
+});
+
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
